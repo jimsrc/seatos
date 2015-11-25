8cc4f3554fe3870be57d107f656f3710866754a4
Jimmy <jimmy.ilws@gmail.com>
2015-11-25 16:23:47 -0300

    fix for rsync

--- mcs/src/showme_group.py
+++ mcs/src/showme_group.py
@@ -5,8 +5,8 @@ from scipy.io.netcdf import netcdf_file
 from datetime import datetime, time, timedelta
 #------------ shared libraries:
 """
---- antes de modificar cosas, tener en cuenta los bugs en: 
-'../../shared_lib/COMENTARIOS.txt' 
+--- antes de modificar cosas, tener en cuenta los bugs en:
+'../../shared_lib/COMENTARIOS.txt'
 """
 import sys
 sys.path.append('../../shared_lib')
@@ -31,12 +31,15 @@ gral.fnames = fnames = {}
 fnames['ACE']       = '%s/data_ace/64sec_mag-swepam/ace.1998-2014.nc' % HOME
 fnames['McMurdo']   = '%s/actividad_solar/neutron_monitors/mcmurdo/mcmurdo_utc_correg.dat' % HOME
 #fnames['table_richardson']  = '../../../../data_317events_iii.nc'
-fnames['table_richardson']  = '%s/ASOC_ICME-FD/icmes_richardson/data/data_317events_iii.nc' % HOME
+#fnames['table_richardson']  = '%s/ASOC_ICME-FD/icmes_richardson/data/data_317events_iii.nc' % HOME
+fnames['table_richardson']  = '%s/ASOC_ICME-FD/icmes_richardson/data/rich_events_ace.nc' % HOME
 
 #---- directorios de salida
 gral.dirs =  dirs   = {}
 dirs['dir_plots']   = '../plots'
 dirs['dir_ascii']   = '../ascii'
+dirs['suffix']      = '_test_Vmc_'    # sufijo para el directorio donde guardare
+                                    # estas figuras
 
 #-------------------------------------------------------------
 #------- seleccionamos MCs con label-de-catalogo (lepping=2, etc)
@@ -49,23 +52,23 @@ dirs['dir_ascii']   = '../ascii'
 MCwant  = {'flags':     ('2',),
            'alias':     '2'}            # para "flagear" el nombre/ruta de las figuras
 
-FILTER                   = {}
-FILTER['Mcmultiple']     = False # True para incluir eventos multi-MC
-FILTER['MCwant']         = MCwant
-FILTER['CorrShift']      = True
-FILTER['wang']           = False #False #True
-FILTER['vsw_filter']     = False #True
-FILTER['z_filter_on']    = False
-FILTER['B_filter']       = True
+FILTER                  = {}
+FILTER['Mcmultiple']    = False # True para incluir eventos multi-MC
+FILTER['CorrShift']     = True
+FILTER['wang']          = True #False #True
+FILTER['vsw_filter']    = True
+FILTER['z_filter_on']   = False
+FILTER['MCwant']        = MCwant
+FILTER['B_filter']      = False
 FILTER['filter_dR.icme'] = False #True
 
-CUTS                            = {}
-CUTS['ThetaThres']              = 90.0 # all events with theta>ThetaThres
-CUTS['dTday']                   = 0.0
-CUTS['v_lo'], CUTS['v_hi']      = 550.0, 3000.0
-CUTS['z_lo'], CUTS['z_hi']      = -50.0, 0.65 
-CUTS['B_lo'], CUTS['B_hi']      = 0.0, 0.0
-CUTS['dR_lo'], CUTS['dR_hi']    = 0.0, 0.0
+CUTS                    = {}
+CUTS['ThetaThres']      = 90.0      # all events with theta>ThetaThres
+CUTS['dTday']           = 0.0
+CUTS['v_lo']            = 550.0
+CUTS['v_hi']            = 3000.0
+CUTS['z_lo']            = -50.0
+CUTS['z_hi']            = 0.65
 
 nBin                    = {}
 nBin['before']          = 2
@@ -76,43 +79,48 @@ fgap                    = 0.2
 
 #--- bordes de estructura
 bounds      = boundaries()
-bounds.tini = tb.tini_mc #tb.tini_mc #tb.tshck 
+bounds.tini = tb.tini_mc #tb.tini_mc #tb.tshck
 bounds.tend = tb.tend_mc #tb.tend_mc #tb.tini_mc
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 gral.data_name      = 'ACE'
 
-FILTER['B_filter']    = False
+FILTER['vsw_filter']    = False
 emgr    = events_mgr(gral, FILTER, CUTS, bounds, nBin, fgap, tb, z_exp)
-emgr.run_all()
 
-emgr.FILTER['B_filter']    = True
-emgr.CUTS['B_lo'], emgr.CUTS['B_hi'] = 0.0, 10.0 #15.0 # 300.0
-emgr.run_all()
+#++++ limites
+LOW, MID1, MID2, TOP = 100.0, 450.0, 550.0, 3000.0
 
-emgr.FILTER['B_filter']    = True
-emgr.CUTS['B_lo'], emgr.CUTS['B_hi'] = 10.0, 15.0
-emgr.run_all()
+emgr.FILTER['vsw_filter']    = True
 
-emgr.FILTER['B_filter']    = True
-emgr.CUTS['B_lo'], emgr.CUTS['B_hi'] = 15.0, 300.0
-emgr.run_all()
+#emgr.CUTS['v_lo'], emgr.CUTS['v_hi'] = LOW, MID1 # 100.0, 450.0
+emgr.CUTS['v_lo'], emgr.CUTS['v_hi'] = MID1, MID2 # 450.0, 550.0
+#emgr.CUTS['v_lo'], emgr.CUTS['v_hi'] = MID2, TOP # 550.0, 3000.0
+#++++++++++++++++++++++++++++++++++++++++++++++++++
+emgr.filter_events()
+ids     = []
+for id in range(emgr.tb.n_icmes):
+    if emgr.SELECC[id]:
+        ids += [id]
+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"""
 emgr.data_name      = 'McMurdo'
 
-emgr.FILTER['B_filter']    = False
+emgr.FILTER['vsw_filter']    = False
 emgr.run_all()
 
-emgr.FILTER['B_filter']    = True
-emgr.CUTS['B_lo'], emgr.CUTS['B_hi'] = 0.0, 10.0
+emgr.FILTER['vsw_filter']    = True
+emgr.CUTS['v_lo'], emgr.CUTS['v_hi'] = LOW, MID1 # 100.0, 450.0
 emgr.run_all()
 
-emgr.FILTER['B_filter']    = True
-emgr.CUTS['B_lo'], emgr.CUTS['B_hi'] = 10.0, 15.0
+emgr.FILTER['vsw_filter']    = True
+emgr.CUTS['v_lo'], emgr.CUTS['v_hi'] = MID1, MID2 # 450.0, 550.0
 emgr.run_all()
 
-emgr.FILTER['B_filter']    = True
-emgr.CUTS['B_lo'], emgr.CUTS['B_hi'] = 15.0, 300.0
+emgr.FILTER['vsw_filter']    = True
+emgr.CUTS['v_lo'], emgr.CUTS['v_hi'] = MID2, TOP # 550.0, 3000.0
 emgr.run_all()
+"""
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
