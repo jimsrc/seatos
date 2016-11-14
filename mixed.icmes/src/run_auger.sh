bd2d84be7b409ecd8d147bd70a82fe8b372f26a3
jimsrc <jimmy.ilws@gmail.com>
2016-11-14 20:51:07 -0300

    pass gap-fraction tolerance in command-line

--- mixed.icmes/src/run_auger.sh
+++ mixed.icmes/src/run_auger.sh
@@ -2,10 +2,16 @@
 export LEFT=$MEAN_PROFILES_ACE/one.structure/src/out.auger/MCflag0.1.2.2H/woShiftCorr/_sh.i_
 export RIGHT=$MEAN_PROFILES_ACE/one.structure/src/out.auger/MCflag0.1.2.2H/woShiftCorr/_i_
 
-group=low
-OUT=./out.auger_${group}
-lims='375. 450.'
-./splitted.py -- --left $LEFT  --right $RIGHT  --plot $OUT  --lim $lims
+group=(lo mid hi)
+lims=('100. 375.' '375. 450.' '450. 3000.')
 
+# splitted profiles
+for i in $(seq 0 1 2); do
+    OUT=./out.auger_${group[$i]}
+    ./splitted.py -- --left $LEFT  --right $RIGHT  --plot $OUT  --lim ${lims[$i]}
+done
 
+# global profile
+OUT=./out.auger_all
+./splitted.py -- --left $LEFT  --right $RIGHT  --plot $OUT
 #EOF
