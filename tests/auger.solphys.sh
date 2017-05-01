c2bb6c91538641fbb6c2d2d195d04ba238eaa464
jimarch <jimmy.ilws@gmail.com>
2017-05-01 18:28:25 -0300

    [tests] new directory for test scripts

---
+++ tests/auger.solphys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export LEFT=$MEAN_PROFILES_ACE/one.structure/src/out.auger/MCflag0.1.2.2H/woShiftCorr/_sh.i_
+export RIGHT=$MEAN_PROFILES_ACE/one.structure/src/out.auger/MCflag0.1.2.2H/woShiftCorr/_i_
+
+group=(lo mid hi)
+lims=('100. 375.' '375. 450.' '450. 3000.')
+EXE=$MEAN_PROFILES_ACE/mixed.icmes/src/splitted.py
+
+# splitted profiles
+for i in $(seq 0 1 2); do
+    OUT=./out.auger_${group[$i]} && mkdir $OUT
+    $EXE -- --left $LEFT  --right $RIGHT  --plot $OUT  --lim ${lims[$i]}
+done
+
+# global profile
+OUT=./out.auger_all
+./splitted.py -- --left $LEFT  --right $RIGHT  --plot $OUT
+#EOF
