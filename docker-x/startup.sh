2f2c3d2e1751fd6f2e7898955c45944cefd401ba
jimarch <jimmy.ilws@gmail.com>
2017-07-22 02:32:14 -0300

    [dockerfile] all we need to build X service from ubuntu:12.04 image

---
+++ docker-x/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Add docker user and generate:
+DOCKER_PASSWORD=docker #`pwgen -c -n -1 12`
+DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'`
+useradd -m -d /home/docker -p $DOCKER_ENCRYPYTED_PASSWORD -s /bin/bash docker
+sed -Ei 's/adm:x:4:/docker:x:4:docker/' /etc/group
+adduser docker sudo
+
+# Copy the config files into the docker directory
+cd /src/config/ && sudo -u docker cp -R .[a-z]* [a-z]* /home/docker/
+
+# restarts the xdm service
+/etc/init.d/xdm restart
+
+#EOF
